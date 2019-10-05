b8a78506172be235d3a91403cbb64b9b09c5eff6
Solovev Vladimir <sovliv4git@yandex.ru>
2019-10-05 17:10:59 +0300

    1) Connected Vue.js. 2) Added simple page layout.

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,24 @@
+// Работа с Vue.js
+Vue.component('message-row',{
+    props: ['message'],
+    template: '<div><i>({{ message.id }}})</i>{{ message.text }}</div>'
+})
+
+// Определяем новый компонент под именем messages-list
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div><message-row v-for="message in messages" :key="message.id" :message="message"/></div>'
+})
+
+//js синтаксис
+var app = new Vue({
+    el: '#app',
+    template: '<messages-list :messages="messages"/>',
+    data: {
+        messages: [
+            {id: '1', text: 'firs'},
+            {id: '2', text: 'second'},
+            {id: '3', text: 'third'}
+        ]
+    }
+});

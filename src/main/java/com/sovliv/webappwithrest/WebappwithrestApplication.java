9a70a8bfc27a4e74e0f9a5539c63a9d52a1a498f
Solovev Vladimir <sovliv4git@yandex.ru>
2019-10-05 12:46:09 +0300

    Loading the generated project from spring initializr.

---
+++ src/main/java/com/sovliv/webappwithrest/WebappwithrestApplication.java
@@ -0,0 +1,13 @@
+package com.sovliv.webappwithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappwithrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappwithrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sovliv/webappwithrest/WebappwithrestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sovliv.webappwithrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebappwithrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

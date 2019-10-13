e13276f20fa2aea71e937f80e6ff4006ea01ff4d
Solovev Vladimir <sovliv4git@yandex.ru>
2019-10-13 17:42:39 +0300

    1) Syntax corrections.

--- src/test/java/com/sovliv/webappwithrest/WebAppWithRestApplicationTests.java
+++ src/test/java/com/sovliv/webappwithrest/WebAppWithRestApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebappwithrestApplicationTests {
+public class WebAppWithRestApplicationTests {
 
 	@Test
 	public void contextLoads() {
